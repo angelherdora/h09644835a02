921effeca4ce3a37526b9f01d922085ac78bbd81
angelherdora <angelherdora@gmail.com>
2023-02-19 23:20:20 -0600

    20230219 - UPDATE CONTENTS

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@
 
 (function () {
   var controllerContact = function controllerContact() {
-    var btnContact = document.getElementById('contact');
+    var btnContact = document.querySelectorAll('.contactActive');
     var modal = document.getElementById('modal'),
       modalClose = document.querySelectorAll('.o-link--close, .o-btn.close'),
       blkSuccess = document.getElementById('success-message'),
       linkReset = document.getElementById('reset');
-    btnContact.addEventListener("click", function (e) {
-      e.preventDefault();
-      if (modal.classList.contains('hidden')) {
-        blkSuccess.classList.remove('sib-form-message-panel--active');
-        document.querySelector('body').style['overflow'] = 'hidden';
-        modal.classList.remove('hidden');
-      }
-    }, false);
+
+    // btnContact.addEventListener("click",function(e){
+    // 	e.preventDefault();
+    // 	if (modal.classList.contains('hidden')) {
+    // 		blkSuccess.classList.remove('sib-form-message-panel--active');
+    // 		document.querySelector('body').style['overflow'] = 'hidden';
+    // 		modal.classList.remove('hidden');
+    // 	}
+    // },false);
+
     linkReset.addEventListener("click", function (e) {
       e.preventDefault();
       blkSuccess.classList.remove('sib-form-message-panel--active');
@@ -25,6 +27,16 @@
         if (!modal.classList.contains('hidden')) {
           document.querySelector('body').style['overflow'] = 'initial';
           modal.classList.add('hidden');
+        }
+      });
+    });
+    btnContact.forEach(function (item) {
+      item.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (modal.classList.contains('hidden')) {
+          blkSuccess.classList.remove('sib-form-message-panel--active');
+          document.querySelector('body').style['overflow'] = 'hidden';
+          modal.classList.remove('hidden');
         }
       });
     });
